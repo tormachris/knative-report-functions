cd3249c51fb91a7afd5fc1d484e7192320795db1
Torma Kristóf <tormakristof@tormakristof.eu>
2019-10-07 00:04:46 +0200

    remove return from isprime

--- isprime/isprime.go
+++ isprime/isprime.go
@@ -13,18 +13,18 @@ func handler(w http.ResponseWriter, r *http.Request) {
     target := os.Getenv("TARGET")
     num, err := strconv.Atoi(target)
 	if err != nil {
-		return "", fmt.Errorf("Failed to parse %s as int! %v", target, err)
+		fmt.Errorf("Failed to parse %s as int! %v", target, err)
 	}
 	log.Print("Checking if %s is prime", target)
 	if num <= 1 {
-		return fmt.Sprintf("%d is not prime", num), nil
+		fmt.Sprintf("%d is not prime", num), nil
 	}
 	for i := 2; i <= int(math.Floor(float64(num)/2)); i++ {
 		if num%i == 0 {
-			return fmt.Sprintf("%d is not prime", num), nil
+			fmt.Sprintf("%d is not prime", num), nil
 		}
 	}
-	return fmt.Sprintf("%d is prime", num), nil
+	fmt.Sprintf("%d is prime", num), nil
 }
 
 func main() {
